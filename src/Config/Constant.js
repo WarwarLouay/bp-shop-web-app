9d39d52dd4d9aca8af1d439731ba5e424a15b901
WarwarLouay <warwarlouay@gmail.com>
2023-02-16 15:57:40 +0200

    change server

--- src/Config/Constant.js
+++ src/Config/Constant.js
@@ -1,6 +1,6 @@
 const Constant = {
-    serverlink: 'http://localhost:4000',
-    // serverlink: 'https://smiling-coveralls-crow.cyclic.app/',
+    // serverlink: 'http://localhost:4000',
+    serverlink: 'https://smiling-coveralls-crow.cyclic.app/',
 
     timeout: 40000
 };
